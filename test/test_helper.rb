73ba3403297da8f93b3538b1665706ce3530c9eb
Peter John Alvarado <peterjohn@gorated.ph>
2020-05-12 21:28:14 +0800

    - test - Add test for delivery_fee calculation and placing order - -

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,23 @@ VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
+
+def token
+  "70B4ABCA8A35AC50E1585E27F217DF160F740D46"
+end
+
+def delivery_details
+  {
+    address: "Unit 802, EcoTower, 32nd Street corner 9th Avenue, BGC, Taguig, 1634 Metro Manila",
+    contact_person: { name: "Reciever", phone: "639123456789" },
+    packages: []
+  }
+end
+
+def pickup_details
+  {
+    address: "Capitol Park Homes, 71 Berlin Avenue, Matandang Balara, Quezon City, Metro Manila",
+    contact_person: { name: "Merchant", phone: "639123456789" },
+    packages: []
+  }
+end
