555dbb3d84b384f370ae456a8c6f31f1b9b6f2ac
Peter John Alvarado <peterjohn@gorated.ph>
2020-05-12 17:32:29 +0800

    - feat - Add method for placing order and refactor reusable sections - -

--- lib/mr_speedy_ruby/client.rb
+++ lib/mr_speedy_ruby/client.rb
@@ -21,6 +21,8 @@ module MrSpeedyRuby
     end
 
     # Delivery Fee Calculation
+    # @since 0.1.0
+    #
     # @param [Hash] pickup pickup location
     # @param pickup [String] :address delivery parcel pickup address
     # @param pickup [Hash] :contact_person delivery parcel pickup contact person
@@ -44,25 +46,52 @@ module MrSpeedyRuby
     #
     # @see https://apitest.mrspeedy.ph/business-api/doc#calculate-order
     def calculate_fees(pickup:, delivery:, opts: {}, sandbox: false)
-      pickup_details = pickup.deep_symbolize_keys
-      pickup_details.assert_required_keys(points_keys)
-      pickup_details[:contact_person].to_h.assert_required_keys(contact_person_keys)
-
-      delivery_details = delivery.deep_symbolize_keys
-      delivery_details.assert_required_keys(points_keys)
-      delivery_details[:contact_person].to_h.assert_required_keys(contact_person_keys)
+      validate_delivery(details: pickup)
+      validate_delivery(details: delivery)
 
       opts.deep_symbolize_keys.assert_required_keys(option_keys)
 
       endpoint = "/api/business/#{version}/calculate-order"
       url = build_url(endpoint, sandbox)
-      # Set MOTORBIKE as default vehicle
-      vehicle = opts[:vehicle_type_id] || MOTORBIKE
+      payload = build_payload(opts: opts, pickup: pickup, delivery: delivery)
 
-      payload = opts.merge({
-        vehicle_type_id: vehicle,
-        points: [pickup, delivery]
-      })
+      post(endpoint: url, token: @token, payload: payload)
+    end
+
+    # Place Order
+    # @since 0.1.0
+    #
+    # @param [Hash] pickup pickup location
+    # @param pickup [String] :address delivery parcel pickup address
+    # @param pickup [Hash] :contact_person delivery parcel pickup contact person
+    # details
+    # @param contact_person [String] :name delivery parcel pickup contact person
+    #   name
+    # @param contact_person [String] :phone delivery parcel pickup contact person
+    # contact details(required)
+    #
+    # @param [Hash] delivery location
+    # @param delivery [String] :address delivery parcel address
+    # @param delivery [Hash] :contact_person delivery parcel contact person details
+    # @param contact_person [String] :name delivery parcel contact person name
+    # @param contact_person [String] :phone delivery parcel contact person
+    # contact details(required)
+    #
+    # @param [Hash] opts additional details
+    # @param opts [String] :matter delivery parcel type
+    #
+    # @param [Boolean] sandbox sandbox identifier(default false)
+    #
+    # @see https://apitest.mrspeedy.ph/business-api/doc#create-order
+    def place_order(pickup:, delivery:, opts: {}, sandbox: false)
+      validate_delivery(details: pickup)
+      validate_delivery(details: delivery)
+
+      opts.deep_symbolize_keys.assert_required_keys(option_keys)
+
+      endpoint = "/api/business/#{version}/create-order"
+      url = build_url(endpoint, sandbox)
+      payload = build_payload(opts: opts, pickup: pickup, delivery: delivery)
 
       post(endpoint: url, token: @token, payload: payload)
     end
@@ -70,6 +99,7 @@ module MrSpeedyRuby
     private
 
     # Options Required Keys
+    # @since 0.1.0
     #
     # @private
     #
@@ -88,6 +118,7 @@ module MrSpeedyRuby
     end
 
     # Contact Person Required Keys
+    # @since 0.1.0
     #
     # @private
     #
@@ -103,6 +134,31 @@ module MrSpeedyRuby
     # @return [Array<String>]
     def package_keys
       %i[ware_code description items_count item_payment_amount]
+    end
+
+    # Delivery Details Validator
+    # @since 0.1.0
+    #
+    # @private
+    def validate_delivery(details:)
+      delivery_details = details.deep_symbolize_keys
+      delivery_details.assert_required_keys(points_keys)
+      delivery_details[:contact_person].to_h.assert_required_keys(contact_person_keys)
+    end
+
+    # Payload Builder
+    # @since 0.1.0
+    #
+    # @private
+    #
+    # @return [Hash]
+    def build_payload(opts:, pickup:, delivery:)
+      # Set MOTORBIKE as default vehicle
+      vehicle = opts[:vehicle_type_id] || MOTORBIKE
+      opts.merge({
+        vehicle_type_id: vehicle,
+        points: [pickup, delivery]
+      })
     end
   end
 end
